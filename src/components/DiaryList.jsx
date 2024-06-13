bd96a4965dacccfa8ad2e0feac071452dcc468d3
seyong <tpdyd0517@gmail.com>
2024-06-13 15:49:01 +0900

    initial commit

---
+++ src/components/DiaryList.jsx
@@ -0,0 +1,50 @@
+import "./DiaryList.css";
+import Button from "./Button";
+import DiaryItem from "./DiaryItem";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const DiaryList = ({ data }) => {
+  const nav = useNavigate();
+  const [sortType, setSortType] = useState("latest");
+
+  const onChangeSortType = (e) => {
+    setSortType(e.target.value);
+  };
+
+  const getSortedDate = () => {
+    // 원본배열을 건드리지않고 정렬후 새로운배열 반환
+    return data.toSorted((a, b) => {
+      if (sortType === "oldest") {
+        return Number(a.createdDate) - Number(b.createdDate);
+      } else {
+        return Number(b.createdDate) - Number(a.createdDate);
+      }
+    });
+  };
+
+  const sortedData = getSortedDate();
+
+  return (
+    <div className="DiaryList">
+      <div className="menu_bar">
+        <select onChange={onChangeSortType}>
+          <option value={"latest"}>최신순</option>
+          <option value={"oldest"}>오래된 순</option>
+        </select>
+        <Button
+          onClick={() => nav("/new")}
+          text={"새일기 쓰기"}
+          type={"POSITIVE"}
+        />
+      </div>
+      <div className="list_wrapper">
+        {sortedData.map((item) => (
+          <DiaryItem key={item.id} {...item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DiaryList;
---
+++ src/hooks/useDiary.jsx
@@ -0,0 +1,27 @@
+import { useContext, useState, useEffect } from "react";
+import { DiaryStateContext } from "../App";
+import { useNavigate } from "react-router-dom";
+
+const useDiary = (id) => {
+  const data = useContext(DiaryStateContext);
+  const [curDiaryItem, setCurDiaryItem] = useState();
+  const nav = useNavigate();
+
+  useEffect(() => {
+    const currentDiaryItem = data.find(
+      (item) => String(item.id) === String(id)
+    );
+
+    if (!currentDiaryItem) {
+      window.alert("존재하지 않는 일기입니다.");
+      // 컴포넌트 생성전이기때문에(return 전) nav 작동불가
+      nav("/", { replace: true });
+    }
+
+    setCurDiaryItem(currentDiaryItem);
+  }, [id, data]);
+
+  return curDiaryItem;
+};
+
+export default useDiary;
